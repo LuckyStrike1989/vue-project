96005757bede6ed4dadc9bf4d71251c3e7434615
= <maestrois@naver.com>
2025-02-26 23:29:55 +0900

    mybatis 설치 및 적용

--- APIServer/src/api/v1/lunchmenu.js
+++ APIServer/src/api/v1/lunchmenu.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const getDBConnection = require('../../db/mariadb/connection')
+const getDBConnection = require('../../db/mariadb/connection');
+const mybatisMapper = require('mybatis-mapper');
+
+// XML 파일 경로는 상대 경로가 아닌 절대 경로(Root 경로)로 './' 또는 '/' 없이 입력해야 합니다.
+mybatisMapper.createMapper(['./src/db/mappers/mariadb/lunchmenu/lunchmenu-sql.xml']);
+
+// SQL 언어 형식과 들여쓰기를 설정
+const format = {language: 'mariadb', indent: ' '};
+
+// mybatisMapper namespace
+const namespace = 'apiserver.db.mappers.mariadb.lunchmenu';
 
 // 전체 점심 메뉴 리스트를 리턴합니다.
 router.get('/', async function(req, res, next) {
@@ -13,13 +23,16 @@ router.get('/', async function(req, res, next) {
         data: []
     }
 
-    const query = "SELECT * FROM tb_lunch_menu ORDER BY REG_DTM DESC";
+    // const query = "SELECT * FROM tb_lunch_menu ORDER BY REG_DTM DESC";
+	const query = mybatisMapper.getStatement(namespace, 'selectLunchMenuList', {}, format)
 
     let conn;
 
     try {
         conn = await getDBConnection();
-        data = await conn.query(query, []);
+        // data = await conn.query(query, []);
+		data = await conn.query(query);
+
         if( data.length == 0 ) {
             result.message = "데이터가 없습니다.";
         } else {
@@ -55,11 +68,20 @@ router.get('/:no', async function(req, res, next) {
         data: {}
     };
 
-    const query = "SELECT * FROM tb_lunch_menu WHERE seq = ?";
+	const params = {
+		seq : req.params.no
+	};
+
+    // const query = "SELECT * FROM tb_lunch_menu WHERE seq = ?";
+	const query = mybatisMapper.getStatement(namespace, 'selectLunchMenuItem', params, format);
+
     let conn;
+
     try {
         conn = await getDBConnection();
-        data = await conn.query(query, [req.params.no]);
+        // data = await conn.query(query, [req.params.no]);
+		data = await conn.query(query);
+
         if( data.length == 0 ) {
             result.message = "데이터가 없습니다.";
         } else {
@@ -103,12 +125,20 @@ router.post('/', async function(req, res, next) {
 		return;
 	}
 
-	const query = "INSERT INTO tb_lunch_menu (menu_name) VALUES(?)";
+	const params = {
+		menuName : req.body.menuName
+	};
+
+	// const query = "INSERT INTO tb_lunch_menu (menu_name) VALUES(?)";
+	const query = mybatisMapper.getStatement(namespace, 'isnertLunchMenuItem', params, format);
 
 	let conn;
+
 	try {
 		conn = await getDBConnection();
-		data = await conn.query(query, [req.body.menuName]);
+		// data = await conn.query(query, [req.body.menuName]);
+		data = await conn.query(query);
+
 		// OkPacket { affectedRows: 1, insertId: 4n, warningStatus: 0 }
 		if (data.affectedRows == 1) {
 			result.message = "데이터가 등록되었습니다.";
@@ -152,12 +182,21 @@ router.put('/:no', async function(req, res, next) {
 		return;
 	}
 
-    const query = "UPDATE tb_lunch_menu SET menu_name = ? WHERE seq = ?";
+	const params = {
+		menuName : req.body.menuName,
+		seq : req.params.no
+	};
+
+    // const query = "UPDATE tb_lunch_menu SET menu_name = ? WHERE seq = ?";
+	const query = mybatisMapper.getStatement(namespace, 'updateLunchMenuItem', params, format);
     
 	let conn;
+
 	try {
 		conn = await getDBConnection();
-		data = await conn.query(query, [req.body.menuName, req.params.no]);
+		// data = await conn.query(query, [req.body.menuName, req.params.no]);
+		data = await conn.query(query);
+
 		// OkPacket { affectedRows: 1, insertId: 0n, warningStatus: 0 }
 		if (data.affectedRows == 1) {
 			result.message = "데이터가 수정되었습니다.";
@@ -193,12 +232,19 @@ router.delete('/:no', async function(req, res, next) {
 		data: {}
 	}
 
-	const query = "DELETE FROM tb_lunch_menu WHERE seq = ?";
+	const params = {
+		seq : req.params.no
+	};
+
+	// const query = "DELETE FROM tb_lunch_menu WHERE seq = ?";
+	const query = mybatisMapper.getStatement(namespace, 'deleteLunchMenuItem', params, format);
     
 	let conn;
 	try {
 		conn = await getDBConnection();
-		data = await conn.query(query, [req.params.no]);
+		// data = await conn.query(query, [req.params.no]);
+		data = await conn.query(query);
+
 		// OkPacket { affectedRows: 1, insertId: 0n, warningStatus: 0 }
 		if (data.affectedRows == 1) {
 			result.message = "데이터가 삭제되었습니다.";
